7938cea8f5c828fda23b33c2c86dde5eb62dcd17
Sam-Banankhu <sambanankhumhango@gmail.com>
2023-07-10 13:35:04 +0200

    added some config and tested end points

--- api/models.py
+++ api/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+class Region(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=10)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        app_label = 'api'
+
+class District(models.Model):
+    name = models.CharField(max_length=255)
+    code = models.CharField(max_length=10)
+    region = models.ForeignKey(Region, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        app_label = 'api'
+
+class City(models.Model):
+    name = models.CharField(max_length=255)
+    district = models.ForeignKey(District, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        app_label = 'api'
--- metamw/urls.py
+++ metamw/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('/', admin.site.urls),
     # Other URL patterns of your project
     path('api/', include('api.urls')),
 ]
